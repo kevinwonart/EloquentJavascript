60a6c5ae3651fd10edfe12d561370132e4f61bde
Kevin Won <kevin.won91@gmail.com>
2023-09-23 09:46:01 -0500

    complete chapter 5 Higher Order Function

--- 05_HigherOrderFunction.js
+++ 05_HigherOrderFunction.js
@@ -143,4 +143,88 @@ console.log(f10(9));//false
 console.log(!f10(11));//false
 console.log(!f10(9));//true
 
-console.log(SCRIPTS[1].name);
+console.log(SCRIPTS[0].name);
+
+
+function filter(array, test) {
+    let passed = [];
+    for (let element of array) {
+        if (test(element)) {
+            passed.push(element);
+        }
+    }
+    return passed;
+}
+//console.log(filter(SCRIPTS, script => script.living)); //[ { name: "Adlam ..."
+
+function characterScript(code) {
+    for (let script of SCRIPTS) {
+        if (script.ranges.some(([from, to]) => {
+            return code >= from && code < to;
+        })) {
+            return script;
+        }
+    }
+    return null;
+}
+
+console.log(characterScript(121)); // { name: 'Latin', ranges: ...
+
+function countBy(items, groupName) {
+    let counts = [];
+    for (let item of items) {
+        let name = groupName(item);
+        let known = counts.findIndex(c => c.name == name);
+        if (known == -1){
+            counts.push({name, count: 1});
+        } else {
+            counts[known].count++;
+        }
+    }
+    return counts;
+}
+
+console.log(countBy([1,2,3,4,5], n => n > 2)); // [ { name: false, count: 2 }, { name: true, count: 3 } ]
+
+function dominantDirection(string){
+    let str = string;
+
+    //remove all characters not found in script object
+    for(let i = 0; i < str.length; i++) 
+        if(characterScript(str.charCodeAt(i)) == null){
+            str = str.slice(0, i) + str.slice(i + 1, str.length-1);
+            i--;
+        }
+
+    let counts = countBy(str, n => characterScript(n.charCodeAt(0)).direction);
+    if( counts.length > 1) {
+        return counts.reduce((a, b) => {
+            return a.count > b.count ? a.name : b.name;
+        });
+    } else
+        return counts[0].name;
+}
+
+//author's solution
+function dominantDirection2(text) {
+    let counted = countBy(text, char => {
+      let script = characterScript(char.codePointAt(0));
+      return script ? script.direction : "none";
+    }).filter(({name}) => name != "none");
+
+    if (counted.length == 0) return "ltr";
+
+    return counted.reduce((a, b) => a.count > b.count ? a : b).name;
+}
+
+console.log(dominantDirection("He,llo!"));//ltr
+console.log(dominantDirection("Hey, مساء الخير"));//rtl
+
+
+/*
+var name = "False";
+var counts = [];
+counts.push({name, count : 1});
+counts.push({name, count : 1});
+console.log(counts);
+*/
