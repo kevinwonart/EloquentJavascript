73e13e629422f6083e1ec1c1e68f8cf4370d6341
Kevin Won <kevin.won91@gmail.com>
2023-09-24 13:39:41 -0500

    More of Chapter 6

--- 06_TheSecretLifeofObjects.js
+++ 06_TheSecretLifeofObjects.js
@@ -108,8 +108,8 @@ class Rabbit2 {
     }
 }
 
-let killerRabbit2 = new Rabbit("killer");
-let blackRabbit = new Rabbit("black");
+let killerRabbit2 = new Rabbit2("killer");
+let blackRabbit = new Rabbit2("black");
 
 //at the time of writing of this book (2019) only methods can be
 //instantiated or constructed in the class declaration...
@@ -118,3 +118,136 @@ let blackRabbit = new Rabbit("black");
 
 let object = new class { getWord() { return "Hello"; } };
 console.log(object.getWord());//Hello
+
+//Overriding Derived Properties pg 103
+Rabbit2.prototype.teeth = "small"
+console.log(killerRabbit2.teeth);//small
+killerRabbit2.teeth = "long,sharp, and bloody";
+console.log(killerRabbit2.teeth);//long, sharp and bloody
+console.log(blackRabbit.teeth);//small
+console.log(Rabbit2.prototype.teeth);//small
+
+console.log(Array.prototype.toString == Object.prototype.toString);//false
+console.log([1, 2].toString());//1,2
+console.log(Object.prototype.toString.call([1, 2]));//[object Array]
+
+//Maps page 104
+let ages = {
+    Boris: 39,
+    Liang: 22,
+    Julia: 62
+};
+
+console.log(`Julia is ${ages["Julia"]}`);//Julia is 62
+console.log("Is Jack's age known?", "Jack" in ages);//Is Jack's age known?" false
+console.log("Is toString's age known?", "toString" in ages);//Is toString's age known? true
+
+console.log("toString" in Object.create(null));//false
+console.log(ages in Object.create(null));//false
+Object.create(null);
+console.log("toString" in Object.create(null));//false note: i don't understand Object.create(null) to pass an Object not derived from Object.prototype
+
+let agesMap = new Map();
+agesMap.set("Boris", 39);
+agesMap.set("Liang", 22);
+agesMap.set("Julia", 62);
+
+console.log(`Julia is ${agesMap.get("Julia")}`);//Julia is 62
+console.log("Is Jack's age known?", agesMap.has("Jack"));//Is Jack's age known?" false
+console.log(agesMap.has("toString"));//false
+
+console.log({x: 1}.hasOwnProperty("x"));//true
+console.log({x: 1}.hasOwnProperty("toString"));//false
+
+//Polymorphism pg 105
+Rabbit2.prototype.toString = function() {
+    return `a ${this.type} rabbit`;
+};
+console.log(String(blackRabbit));
+
+//Symbols pg 106
+let sym = Symbol("name");
+console.log(sym == Symbol("name"));//false
+Rabbit2.prototype[sym] = 55;
+console.log(blackRabbit[sym]);//55
+
+let sym2 = Symbol("name");
+Rabbit2.prototype[sym2] = 56;
+console.log(blackRabbit[sym2]);//56
+
+const toStringSymbol = Symbol("toString");
+Array.prototype[toStringSymbol] = function() {
+    return `${this.length} cm of blue yarn`;
+};
+
+console.log([1, 2].toString());//1,2
+console.log([1, 2][toStringSymbol]());//2 cm of blue yarn
+//symbols allows me to redefine a toString method that doesn't
+//override the prototype method
+//whynot just make a new function with a different name without
+//having to go through the hassle of making a Symbol?
+
+let stringObject = {
+    [toStringSymbol]() {return "a jute rope"; }
+};
+console.log(stringObject[toStringSymbol]());// a jute rope
+
+//The Iterator Interface pg 107
+let okIterator = "OK"[Symbol.iterator]();
+console.log(okIterator.next());//{ value: 'O', done: false }
+console.log(okIterator.next());//{ value: 'K', done: false }
+console.log(okIterator.next());//{ value: undefined, done: false }
+
+class Matrix {
+    constructor(width, height, element = (x, y) => undefined) {
+        this.width = width;
+        this.height = height;
+        this.content = [];
+
+        for (let y = 0; y < height; y++){
+            for (let x = 0; x < width; x++) {
+                this.content[y * width + x] = element(x, y);
+            }
+        }
+    }
+
+    get(x, y) {
+        return this.content[y * this.width + x];
+    }
+    set(x, y, value) {
+        this.content[y * this.width + x] = value;
+    }
+}
+
+class MatrixIterator {
+    constructor(matrix) {
+        this.x = 0;
+        this.y = 0;
+        this.matrix = matrix;
+    }
+
+    next() {
+        if (this.y == this.matrix.height) return {done: true};
+
+        let value = {x: this.x,
+            y: this.y,
+            value: this.matrix.get(this.x, this.y)};
+        this.x++;
+
+        if (this.x == this.matrixwidth) {
+            this.x = 0;
+            this.y++;
+        }
+        return {value, done: false};
+    }
+}
+
+Matrix.prototype[Symbol.iterator] = function() {
+    return new MatrixIterator(this);
+};
+
+let matrix = new Matrix(2, 2, (x, y) => `${x},${y}`);
+for (let {x, y, value} of matrix) {
+    console.log("over here");
+    console.log(x, y, value);
+}
